765f0d8fd2fb041dd773ad845983ca43dc898bd3
craynex <cris2007.martin@gmail.com>
2024-07-11 08:44:36 +0200

    Last commit

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,19 +1,78 @@
 import React from 'react';
+import Slider from 'react-slick';
 import './TripList.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const TripList = ({ trips, onDelete }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="trip-list">
-      {trips.map(trip => (
-        <div key={trip.id} className="trip-card">
-          <h3>{trip.destination}</h3>
-          <p>Flight: {trip.flight}</p>
-          <p>{trip.description}</p>
-          <button className="delete-button" onClick={() => onDelete(trip.id)}>X</button>
-        </div>
-      ))}
+      <Slider {...settings}>
+        {trips.map(trip => (
+          <div key={trip.id} className="trip-card">
+            <h3>{trip.destination}</h3>
+            <p>Flight: {trip.flight}</p>
+            <p>{trip.description}</p>
+            <button className="delete-button" onClick={() => onDelete(trip.id)}>X</button>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "transparent" }}
+      onClick={onClick}
+    >
+      <i className="arrow right" />
+    </div>
+  );
+}
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "transparent" }}
+      onClick={onClick}
+    >
+      <i className="arrow left" />
+    </div>
+  );
+}
 
 export default TripList;
